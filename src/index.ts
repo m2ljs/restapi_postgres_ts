70c72f78ab0a62e02456adc78a53274ebbf98305
mrl00 <m2l_0@outlook.com>
2021-03-15 17:42:24 -0300

    add person router to index

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import helloRouter from './containers/hello/helloRoutes'
+import personRoutes from './containers/person/personRoutes'
 
 const app = express()
 app .use(helloRouter)
+    .use(personRoutes)
     .use(express.json)
     .use(express.urlencoded({extended: false}))
 
